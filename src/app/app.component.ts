51ae320261fc6ad41de46fec6f20662e2fee967b
Mikel Barcina <mbarcina001@gmail.com>
2020-12-28 23:17:55 +0100

    update #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,7 @@ export class AppComponent {
 		  titulado "Aprendizaje semisupervisado: Una aplicación en la detección de entidades médicas".</p>
 		  <p> Además de lo enseñado en la carrera también aprendí por mi cuenta otros lenguajes de programación que me resultaban interesantes 
 		  como AngularJS, React, Ruby y Spring entre otros.</p>`,
-		  "/Portfolio/assets/images/logos/ehu.png"
+		  "./assets/images/logos/ehu.png"
 		),
 		new Trayectoria(
 		  "Informática de Euskadi",
@@ -33,7 +33,7 @@ export class AppComponent {
 		  <p><span class="font-weight-bold">Minisite de MediaMarkt:</span> Realizado en Java y Struts se trata de una aplicación web dedicada a la contratación de la tarjeta MediaMarkt Club Card. Incluye servicios REST así como una estructura multimarca diseñada especificamente con el objetivo de poder alojar otras marcas (Gas natural, Ventajon, etc) de modo que tuviesen un diseño propio y unos pasos para realizar la solicitud a elección del cliente.</p>
 		  <p><span class="font-weight-bold">Digitalización TCF:</span> Aplicación web implementada en Angular4 para la compra de dispositivos móviles financiados por Telefónica Consumer Finance. Este desarrollo cuenta con un Middleware programado en Java que hace las llamadas correspondientes a los servicios que sirven de apoyo a la aplicación. Dicho Middleware expone los servicios necesarios en REST para que puedan ser invocados desde el código Angular4 y realiza diferentes llamadas a servicios empleando Axis, Jaxb y Soap con cabecera de seguridad y firma entre otros.</p>
 		  <p>Por otra parte, hice otros proyectos de menor envergadura con otros lenguajes de programación como Spring, así como diferentes plugins de Java como Selenium.</p>`,
-		  "/Portfolio/assets/images/logos/ide.png"
+		  "./assets/images/logos/ide.png"
 		),
 		new Trayectoria(
 		  "Getronics",
@@ -42,14 +42,15 @@ export class AppComponent {
 		  `<p>Realicé el proyecto <span class="font-weight-bold">FMS (Field Management Services)</span>, una aplicación multiplataforma para Android, iOS y navegador implementada mediante Angular 6, Ionic 3 y Cordova.</p>
 		  <p>Una vez finalizado dicho proyecto, me incorporé al desarrollo de una web para <span class="font-weight-bold">Celsa</span> implementada en AngularJS, con el uso de Gulp para el preprocesado de los estilos y los scripts, y Protractor para los tests unitarios.</p>
 		  <p>También he estado involucrado en la maquetación de la nueva página web de <span class="font-weight-bold">Booker</span> (HTML5, CS3 y Bootstrap).</p>`,
-		  "/Portfolio/assets/images/logos/getronics.png"
+		  "./assets/images/logos/getronics.png"
 		),
 		new Trayectoria(
 		  "Bilbomática",
 		  "Programador",
 		  "2019-Actualidad",
-		  `<p>Me he incorporado a la parte front del proyecto ECCAIRS2 para la Agencia Europea de Seguridad Aérea (EASA) que está siendo desarrollado con Angular 8 y NGRX. </p>`,
-		  "/Portfolio/assets/images/logos/bilbomatica.png"
+		  `<p>Me he incorporado a la parte front del proyecto <span class="font-weight-bold">Eccairs 2</span> para la <span class="font-weight-bold">Agencia Europea de Seguridad Aérea</span> (EASA) que está siendo desarrollado con Angular 8 y Java Spring. El cometido de esta aplicación es la recopilación de incidencias que puedan darse en la aviación y la gestión de la taxonomía empleada en los informes que recogen estas incidencias así como otras gestiones comunes en este tipo de aplicaciones como roles, usuarios, etc.</p>
+		  <p>Cabe destacar también que en este proyecto se ha utilizado la metodología de trabajo SCRUM. </p>`,
+		  "./assets/images/logos/bilbomatica.png"
 		)
 	];
 
@@ -59,14 +60,14 @@ export class AppComponent {
 			"Webs CCF",
 			"Mantenimiento y atención de incidencias para las webs de servicios para CaixaBank Consumer Finance.",
 			[
-				"/Portfolio/assets/images/proyectos/ccf1.PNG",
-				"/Portfolio/assets/images/proyectos/ccf2.PNG",
-				"/Portfolio/assets/images/proyectos/ccf3.PNG",
-				"/Portfolio/assets/images/proyectos/ccf4.PNG",
-				"/Portfolio/assets/images/proyectos/ccf5.PNG",
-				"/Portfolio/assets/images/proyectos/ccf6.PNG",
-				"/Portfolio/assets/images/proyectos/ccf7.PNG",
-				"/Portfolio/assets/images/proyectos/ccf8.PNG"
+				"./assets/images/proyectos/ccf1.PNG",
+				"./assets/images/proyectos/ccf2.PNG",
+				"./assets/images/proyectos/ccf3.PNG",
+				"./assets/images/proyectos/ccf4.PNG",
+				"./assets/images/proyectos/ccf5.PNG",
+				"./assets/images/proyectos/ccf6.PNG",
+				"./assets/images/proyectos/ccf7.PNG",
+				"./assets/images/proyectos/ccf8.PNG"
 			],
 			0,
 			[
@@ -84,17 +85,17 @@ export class AppComponent {
 			"Minisite finonline",
 			"Página web para solicitar la tarjeta de MediaMarkt, Gas Natural, Ventajon, etc.",
 			[
-				"/Portfolio/assets/images/proyectos/mm1.PNG",
-				"/Portfolio/assets/images/proyectos/gn1.PNG",
-				"/Portfolio/assets/images/proyectos/ve1.PNG",
-				"/Portfolio/assets/images/proyectos/mm2.PNG",
-				"/Portfolio/assets/images/proyectos/mm3.PNG",
-				"/Portfolio/assets/images/proyectos/mm4.PNG",
-				"/Portfolio/assets/images/proyectos/mm5.PNG",
-				"/Portfolio/assets/images/proyectos/mm6.PNG",
-				"/Portfolio/assets/images/proyectos/mm7.PNG",
-				"/Portfolio/assets/images/proyectos/mm8.PNG",
-				"/Portfolio/assets/images/proyectos/mm9.PNG"
+				"./assets/images/proyectos/mm1.PNG",
+				"./assets/images/proyectos/gn1.PNG",
+				"./assets/images/proyectos/ve1.PNG",
+				"./assets/images/proyectos/mm2.PNG",
+				"./assets/images/proyectos/mm3.PNG",
+				"./assets/images/proyectos/mm4.PNG",
+				"./assets/images/proyectos/mm5.PNG",
+				"./assets/images/proyectos/mm6.PNG",
+				"./assets/images/proyectos/mm7.PNG",
+				"./assets/images/proyectos/mm8.PNG",
+				"./assets/images/proyectos/mm9.PNG"
 			],
 			0,
 			[
@@ -113,13 +114,13 @@ export class AppComponent {
 			"Digitalización Telefónica Consumer Finance",
 			"Página web para financiar diversos teléfonos móviles con TCF",
 			[
-				"/Portfolio/assets/images/proyectos/dig1.PNG",
-				"/Portfolio/assets/images/proyectos/dig2.PNG",
-				"/Portfolio/assets/images/proyectos/dig3.PNG",
-				"/Portfolio/assets/images/proyectos/dig4.PNG",
-				"/Portfolio/assets/images/proyectos/dig5.PNG",
-				"/Portfolio/assets/images/proyectos/dig6.PNG",
-				"/Portfolio/assets/images/proyectos/dig7.PNG"
+				"./assets/images/proyectos/dig1.PNG",
+				"./assets/images/proyectos/dig2.PNG",
+				"./assets/images/proyectos/dig3.PNG",
+				"./assets/images/proyectos/dig4.PNG",
+				"./assets/images/proyectos/dig5.PNG",
+				"./assets/images/proyectos/dig6.PNG",
+				"./assets/images/proyectos/dig7.PNG"
 			],
 			0,
 			[
@@ -135,24 +136,24 @@ export class AppComponent {
 			"Getronics FMS",
 			"Aplicación multiplataforma para Android, iOS y navegadores destinada a su uso por parte de los técnicos de campo de Getronics. Recupera los tickets pendientes del técnico y le proporciona información sobre las tareas que tiene el técnico para ese día, las ubicaciones a las que se tiene que desplazar, los componentes necesarios, etc.",
 			[
-				"/Portfolio/assets/images/proyectos/fms1.PNG",
-				"/Portfolio/assets/images/proyectos/fms2.png",
-				"/Portfolio/assets/images/proyectos/fms3.png",
-				"/Portfolio/assets/images/proyectos/fms4.png",
-				"/Portfolio/assets/images/proyectos/fms5.png",
-				"/Portfolio/assets/images/proyectos/fms6.png",
-				"/Portfolio/assets/images/proyectos/fms7.png",
-				"/Portfolio/assets/images/proyectos/fms8.png",
-				"/Portfolio/assets/images/proyectos/fms9.png",
-				"/Portfolio/assets/images/proyectos/fms10.png",
-				"/Portfolio/assets/images/proyectos/fms11.png",
-				"/Portfolio/assets/images/proyectos/fms12.png",
-				"/Portfolio/assets/images/proyectos/fms13.png",
-				"/Portfolio/assets/images/proyectos/fms14.png",
-				"/Portfolio/assets/images/proyectos/fms15.png",
-				"/Portfolio/assets/images/proyectos/fms16.png",
-				"/Portfolio/assets/images/proyectos/fms17.png",
-				"/Portfolio/assets/images/proyectos/fms18.png",
+				"./assets/images/proyectos/fms1.PNG",
+				"./assets/images/proyectos/fms2.png",
+				"./assets/images/proyectos/fms3.png",
+				"./assets/images/proyectos/fms4.png",
+				"./assets/images/proyectos/fms5.png",
+				"./assets/images/proyectos/fms6.png",
+				"./assets/images/proyectos/fms7.png",
+				"./assets/images/proyectos/fms8.png",
+				"./assets/images/proyectos/fms9.png",
+				"./assets/images/proyectos/fms10.png",
+				"./assets/images/proyectos/fms11.png",
+				"./assets/images/proyectos/fms12.png",
+				"./assets/images/proyectos/fms13.png",
+				"./assets/images/proyectos/fms14.png",
+				"./assets/images/proyectos/fms15.png",
+				"./assets/images/proyectos/fms16.png",
+				"./assets/images/proyectos/fms17.png",
+				"./assets/images/proyectos/fms18.png",
 			],
 			9,
 			[
@@ -169,10 +170,10 @@ export class AppComponent {
 			"SMED",
 			"Página web para la gestión de un taller de laminado.",
 			[
-				"/Portfolio/assets/images/proyectos/smed1.PNG",
-				"/Portfolio/assets/images/proyectos/smed2.PNG",
-				"/Portfolio/assets/images/proyectos/smed3.PNG",
-				"/Portfolio/assets/images/proyectos/smed4.PNG"
+				"./assets/images/proyectos/smed1.PNG",
+				"./assets/images/proyectos/smed2.PNG",
+				"./assets/images/proyectos/smed3.PNG",
+				"./assets/images/proyectos/smed4.PNG"
 			],
 			1,
 			[
@@ -191,19 +192,36 @@ export class AppComponent {
 			"Maquetación web: Booker",
 			"Maquetación de la nueva página web de Booker. HTML5, CSS y Bootstrap.",
 			[
-				"/Portfolio/assets/images/proyectos/booker1.png",
-				"/Portfolio/assets/images/proyectos/booker2.png",
-				"/Portfolio/assets/images/proyectos/booker3.png",
-				"/Portfolio/assets/images/proyectos/booker4.png",
-				"/Portfolio/assets/images/proyectos/booker5.png",
-				"/Portfolio/assets/images/proyectos/booker6.png",
-				"/Portfolio/assets/images/proyectos/booker7.png"
+				"./assets/images/proyectos/booker1.png",
+				"./assets/images/proyectos/booker2.png",
+				"./assets/images/proyectos/booker3.png",
+				"./assets/images/proyectos/booker4.png",
+				"./assets/images/proyectos/booker5.png",
+				"./assets/images/proyectos/booker6.png",
+				"./assets/images/proyectos/booker7.png"
 			],
 			0,
 			[],
 			"No",
 			"No"
 		)*/
+		,new Proyecto(
+			"p7",
+			"Eccairs 2",
+			"Aplicación para la recopilación de incidencias en aviación y la gestión de la taxonomía empleada en los informes que recogen estas incidencias así como otras gestiones comunes en este tipo de aplicaciones como roles, usuarios, etc.",
+			[
+				"./assets/images/proyectos/eccairs1.png",
+			],
+			0,
+			[
+				"Parte front implementada con Angular 8, Ngrx y Ngrx/data",
+				"Uso de librerías de apoyo como Bootstrap y Kendo",
+				"Uso de oras tenologías como Spring, Elastic o MySQL",
+				"Metodología SCRUM"
+			],
+			"No",
+			"No"
+		)
 	];
 	
 	proyectosCarrera:Proyecto[] = [
@@ -212,8 +230,8 @@ export class AppComponent {
 			"Juego: Pasapalabra",
 			"Juego realizado en Java del popular concurso de televisión Pasapalabra.",
 			[
-				"/Portfolio/assets/images/proyectos/pasapalabra1.JPG",
-				"/Portfolio/assets/images/proyectos/pasapalabra2.JPG"
+				"./assets/images/proyectos/pasapalabra1.JPG",
+				"./assets/images/proyectos/pasapalabra2.JPG"
 			],
 			1,
 			[],
@@ -225,18 +243,18 @@ export class AppComponent {
 			"Juego: Arkanoid",
 			"Juego Arkanoid desarrollado en JavaScript para navegador web",
 			[
-				"/Portfolio/assets/images/proyectos/arkanoid0.png",
-				"/Portfolio/assets/images/proyectos/arkanoid1.png",
-				"/Portfolio/assets/images/proyectos/arkanoid2.png",
-				"/Portfolio/assets/images/proyectos/arkanoid3.png",
-				"/Portfolio/assets/images/proyectos/arkanoid4.png",
-				"/Portfolio/assets/images/proyectos/arkanoid5.png",
-				"/Portfolio/assets/images/proyectos/arkanoid6.png",
-				"/Portfolio/assets/images/proyectos/arkanoid7.png",
-				"/Portfolio/assets/images/proyectos/arkanoid8.png",
-				"/Portfolio/assets/images/proyectos/arkanoid9.png",
-				"/Portfolio/assets/images/proyectos/arkanoid10.png",
-				"/Portfolio/assets/images/proyectos/arkanoid11.png"
+				"./assets/images/proyectos/arkanoid0.png",
+				"./assets/images/proyectos/arkanoid1.png",
+				"./assets/images/proyectos/arkanoid2.png",
+				"./assets/images/proyectos/arkanoid3.png",
+				"./assets/images/proyectos/arkanoid4.png",
+				"./assets/images/proyectos/arkanoid5.png",
+				"./assets/images/proyectos/arkanoid6.png",
+				"./assets/images/proyectos/arkanoid7.png",
+				"./assets/images/proyectos/arkanoid8.png",
+				"./assets/images/proyectos/arkanoid9.png",
+				"./assets/images/proyectos/arkanoid10.png",
+				"./assets/images/proyectos/arkanoid11.png"
 			],
 			11,
 			[
@@ -254,10 +272,10 @@ export class AppComponent {
 			"Juego: Ruleta de la fortuna",
 			"Juego en Java del popular programa de televisión La ruleta de la fortuna",
 			[
-				"/Portfolio/assets/images/proyectos/ruleta1.JPG",
-				"/Portfolio/assets/images/proyectos/ruleta2.JPG",
-				"/Portfolio/assets/images/proyectos/ruleta3.JPG",
-				"/Portfolio/assets/images/proyectos/ruleta4.JPG"
+				"./assets/images/proyectos/ruleta1.JPG",
+				"./assets/images/proyectos/ruleta2.JPG",
+				"./assets/images/proyectos/ruleta3.JPG",
+				"./assets/images/proyectos/ruleta4.JPG"
 			],
 			1,
 			[],
@@ -269,7 +287,7 @@ export class AppComponent {
 			"Gestor de encierros en San Fermín",
 			"Aplicación en Java para que las ganaderías que participan en San Fermín puedan administrar su participación.",
 			[
-				"/Portfolio/assets/images/proyectos/encierros1.JPG",
+				"./assets/images/proyectos/encierros1.JPG",
 			],
 			0,
 			[
@@ -286,11 +304,11 @@ export class AppComponent {
 			"Aplicación Android: Gestor de Reuniones",
 			"Aplicación en Android para la gestión de reuniones.",
 			[
-				"/Portfolio/assets/images/proyectos/App1.png",
-				"/Portfolio/assets/images/proyectos/App2.png",
-				"/Portfolio/assets/images/proyectos/App3.png",
-				"/Portfolio/assets/images/proyectos/App4.png",
-				"/Portfolio/assets/images/proyectos/App5.png"	
+				"./assets/images/proyectos/App1.png",
+				"./assets/images/proyectos/App2.png",
+				"./assets/images/proyectos/App3.png",
+				"./assets/images/proyectos/App4.png",
+				"./assets/images/proyectos/App5.png"	
 			],
 			2,
 			[
@@ -310,10 +328,10 @@ export class AppComponent {
 			"Detector de entidades en textos médicos",
 			"Aplicación que emplea técnicas de minería de datos (aprendizaje semisupervisado y evaluación no supervisada) para identificar entidades tales como enfermedades y medicamentos en textos médicos.",
 			[
-				"/Portfolio/assets/images/proyectos/TFG1.JPG",
-				"/Portfolio/assets/images/proyectos/tfg2.JPG",
-				"/Portfolio/assets/images/proyectos/tfg3.JPG",
-				"/Portfolio/assets/images/proyectos/tfg4.JPG"
+				"./assets/images/proyectos/TFG1.JPG",
+				"./assets/images/proyectos/tfg2.JPG",
+				"./assets/images/proyectos/tfg3.JPG",
+				"./assets/images/proyectos/tfg4.JPG"
 			],
 			3,
 			[],
@@ -328,9 +346,9 @@ export class AppComponent {
 			"App: Pokedex",
 			"Aplicación multiplataforma que simula una pokédex. Realizada para un proceso de selección.",
 			[
-				"/Portfolio/assets/images/proyectos/pokedex1.jpg",
-				"/Portfolio/assets/images/proyectos/pokedex2.jpg",
-				"/Portfolio/assets/images/proyectos/pokedex3.jpg"
+				"./assets/images/proyectos/pokedex1.jpg",
+				"./assets/images/proyectos/pokedex2.jpg",
+				"./assets/images/proyectos/pokedex3.jpg"
 			],
 			0,
 			[
@@ -347,9 +365,9 @@ export class AppComponent {
 			"Github Issue Search",
 			"Página web para la búsqueda de issues en proyectos de github",
 			[
-				"/Portfolio/assets/images/proyectos/issuesearch1.PNG",
-				"/Portfolio/assets/images/proyectos/issuesearch2.PNG",
-				"/Portfolio/assets/images/proyectos/issuesearch3.PNG"
+				"./assets/images/proyectos/issuesearch1.PNG",
+				"./assets/images/proyectos/issuesearch2.PNG",
+				"./assets/images/proyectos/issuesearch3.PNG"
 			],
 			0,
 			[
@@ -366,14 +384,14 @@ export class AppComponent {
 			"SpringMarket",
 			"Página web para ecommerce",
 			[
-				"/Portfolio/assets/images/proyectos/springmarket1.png",
-				"/Portfolio/assets/images/proyectos/springmarket2.png",
-				"/Portfolio/assets/images/proyectos/springmarket3.png",
-				"/Portfolio/assets/images/proyectos/springmarket4.png",
-				"/Portfolio/assets/images/proyectos/springmarket5.png",
-				"/Portfolio/assets/images/proyectos/springmarket6.png",
-				"/Portfolio/assets/images/proyectos/springmarket7.png",
-				"/Portfolio/assets/images/proyectos/springmarket8.png",
+				"./assets/images/proyectos/springmarket1.png",
+				"./assets/images/proyectos/springmarket2.png",
+				"./assets/images/proyectos/springmarket3.png",
+				"./assets/images/proyectos/springmarket4.png",
+				"./assets/images/proyectos/springmarket5.png",
+				"./assets/images/proyectos/springmarket6.png",
+				"./assets/images/proyectos/springmarket7.png",
+				"./assets/images/proyectos/springmarket8.png",
 			],
 			0,
 			[
